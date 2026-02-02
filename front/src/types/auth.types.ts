1ac2fc4788cf4dbb9a37f1795d8aea4d45bed5ca
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-02 22:56:32 +0900

    [add] authのスキーマを定義

---
+++ front/src/types/auth.types.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const authRequestSchema = z.object({
+  email: z.string("有効なメールアドレスを入力してください"),
+  password: z.string().min(8, "パスワードは8文字以上で入力してください"),
+});
+
+export type authRequest = z.infer<typeof authRequestSchema>;
+
+export interface User {
+  id: string;
+  email: string;
+  password: string;
+  meaLfrequency: number;
+}
+
+export interface AuthResponse {
+  success: boolean;
+  message: string;
+}
+
+export interface AuthError {
+  message: string;
+  errors?: Record<string, string[]>;
+}

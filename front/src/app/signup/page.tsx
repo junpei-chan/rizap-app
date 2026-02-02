66fdeee4a0a0305ef17b5e0fa0e305366d1bba9b
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-03 04:33:28 +0900

    [update] 登録ボタンにtype="button"を追加

--- front/src/app/signup/page.tsx
+++ front/src/app/signup/page.tsx
@@ -50,6 +50,7 @@ export default function Signup() {
           />
         </div>
         <button
+          type="button"
           className="bg-blue-500 text-white"
           onClick={handleSubmit}
         >

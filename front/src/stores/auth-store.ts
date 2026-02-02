54265e9fa35d0cf61a39a6642e198034e028e23d
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-03 01:52:41 +0900

    [update] authStoreにpersistとcleaerAuthを追加

--- front/src/stores/auth-store.ts
+++ front/src/stores/auth-store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware"
 import type { User } from "@/types/auth.types";
 
 interface AuthStore {
@@ -6,14 +7,20 @@ interface AuthStore {
   authToken: string | null;
   isAuthenticated: boolean;
   setAuth: (user: User, authToken: string) => void;
+  clearAuth: () => void;
 }
 
-export const useAuthStore = create<AuthStore>(
+export const useAuthStore = create<AuthStore>()(
+  persist(
   (set) => ({
-    user: null,
-    authToken: null,
-    isAuthenticated: false,
-    setAuth: (user, authToken) => 
-      set({ user, authToken, isAuthenticated: true }),
-  }),
+      user: null,
+      authToken: null,
+      isAuthenticated: false,
+      setAuth: (user, authToken) => 
+        set({ user, authToken, isAuthenticated: true }),
+      clearAuth: () => 
+        set({ user: null, authToken: null, isAuthenticated: false })
+    }),
+    { name: "auth-storage" }
+  )
 );

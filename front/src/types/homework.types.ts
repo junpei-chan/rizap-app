75df7f26eb4bda91011481b0531316e9e65ef4b1
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-03 07:16:01 +0900

    [add] homewordの型を定義

---
+++ front/src/types/homework.types.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const roomRequestSchema = z.array( // 配列形式
+  z.object({
+    homeworkId: z.number().int().min(1).max(8),
+    doneAt: z.number().int(),
+  })
+);
+
+export type RoomRequestSchema = z.infer<typeof roomRequestSchema>;

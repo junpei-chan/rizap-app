f43b70c22b1f8ed58ba57c9db2f1488989c8cd3b
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-03 08:48:43 +0900

    [add] getHomework関数を作成

--- front/src/services/homework/homework-service.ts
+++ front/src/services/homework/homework-service.ts
@@ -1,5 +1,5 @@
-import { publicClient } from "@/lib/axios";
-import { RoomRequest } from "@/types/homework.types";
+import { publicClient, authClient } from "@/lib/axios";
+import { RoomRequest, HomeworkRequest, HomeworkResponse } from "@/types/homework.types";
 import type { ApiResponse } from "@/types/api.types";
 
 export const HomeworkService = {
@@ -7,6 +7,12 @@ export const HomeworkService = {
     const { data } = await publicClient.patch<ApiResponse>("/room",
       params,
     );
+    return data;
+  },
+  getHomework: async (params?: HomeworkRequest) => {
+    const { data } = await authClient.get<HomeworkResponse>("/homework", {
+      params
+    });
     return data;
   },
 }

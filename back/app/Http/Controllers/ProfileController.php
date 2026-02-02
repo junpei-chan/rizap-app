28de126db4a89486d33e22a150db30b5995b5f97
KatoYuta <katoyuta021962@gmail.com>
2026-02-02 23:20:09 +0900

    [add]APIのセットアップ完成。

---
+++ back/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function store(){
+
+    }
+
+    public function edit(){
+        
+    }
+}

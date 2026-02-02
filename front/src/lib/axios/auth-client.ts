2b7b5d207688c586e179363248e998d12a0afb7f
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-03 00:19:05 +0900

    [update] Axiosクライアントをnamed exportに変更

--- front/src/lib/axios/auth-client.ts
+++ front/src/lib/axios/auth-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const authClient = axios.create({
+export const authClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
@@ -36,5 +36,3 @@ authClient.interceptors.response.use(
     return Promise.reject(error);
   }
 )
-
-export default authClient;

a40ae471314f03d650cdcb9207dbefc90f0befc1
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-03 21:17:19 +0900

    [add] サインアップページの作成

--- front/src/app/signup/page.tsx
+++ front/src/app/signup/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useSignup } from "@/hooks/features/auth";
+import { Input, Button, Checkbox, PasswordInput } from "@/components/ui";
 
 export default function Signup() {
   const { mutate, isPending, isError, error } = useSignup();
@@ -16,49 +17,74 @@ export default function Signup() {
   };
 
   return (
-    <main className="flex items-center justify-center">
+    <main className="w-screen h-screen flex items-center justify-center">
       <div className="w-82">
         <h1 className="text-center text-2xl">
-          新規会員登録
+          サインアップ
         </h1>
         <form
+          className="
+            text-text mt-11
+            label: text-[12px]
+          "
           onSubmit={(e) => e.preventDefault()}
         >
-          <div>
-            <label
-              htmlFor="email"
-            >
-              メールアドレス
-            </label>
-            <input
-              id="email"
-              type="email"
-              className="border"
-              autoComplete="off"
-              onChange={(e) => setEmail(e.target.value)}
-            />
+          <div className="flex flex-col gap-4.5">
+            <div className="flex flex-col gap-1">
+              <label
+                htmlFor="email"
+                className="pl-1"
+              >
+                メールアドレス
+              </label>
+              <Input
+                id="email"
+                type="email"
+                className="border h-10"
+                autoComplete="off"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="pl-1"
+              >
+                パスワード
+              </label>
+              <PasswordInput
+                id="password"
+                className="border h-10"
+                autoComplete="off"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
           </div>
-          <div>
-            <label
-              htmlFor="password"
-            >
-              パスワード
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="border"
-              autoComplete="off"
-              onChange={(e) => setPassword(e.target.value)}
-            />
+          <div className="flex flex-col gap-3.5 items-center my-6">
+            <div className="flex gap-4.5 text-[12px] justify-center">
+              <p>利用規約</p>
+              <p>プライバシーポリシー</p>
+            </div>
+            <div className="flex gap-1 items-center">
+              <Checkbox 
+                id="terms"
+                className="data-[state=checked]:bg-[#108FFF] data-[state=checked]:border-[#108FFF]" 
+              />
+              <label 
+                htmlFor="terms" 
+                className="cursor-pointer"
+              >
+                上記に同意する
+              </label>
+            </div>
           </div>
-          <button
+          <Button
             type="button"
-            className="bg-blue-500 text-white"
+            className="w-full h-10 bg-[#108FFF] text-white shadow-[0_3px_0px_#0F5B9E]"
             onClick={handleSubmit}
           >
             登録
-          </button>
+          </Button>
         </form>
         {isPending && (
           <p>処理中...</p>
--- front/src/components/ui/index.ts
+++ front/src/components/ui/index.ts
@@ -1,2 +1,4 @@
 export * from "./button";
 export * from "./input";
+export * from "./password-input";
+export * from "./checkbox";

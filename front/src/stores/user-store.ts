652f490871f4b0b35933602f8ea67a6d510ca584
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-03 21:58:02 +0900

    [add] user-storeからHouseworkTypeを分離

--- front/src/stores/user-store.ts
+++ front/src/stores/user-store.ts
@@ -1,11 +1,15 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { User } from "@/types/auth.types";
+import { HouseworkType, HouseworkFrequencies } from "@/types/user.types";
 
 interface UserStore {
   user: User | null;
   mealFrequency: number | null;
+  houseworkFrequencies: HouseworkFrequencies | null;
+  setMealFrequency: (frequency: number) => void;
   setUser: (user: User) => void;
+  setHouseworkFrequency: (type: HouseworkType, frequency: number) => void;
   clearUser: () => void;
 }
 
@@ -14,10 +18,24 @@ export const useUserStore = create<UserStore>()(
     (set) => ({
       user: null,
       mealFrequency: null,
+      houseworkFrequencies: null,
+      setMealFrequency: (frequency) =>
+        set({ mealFrequency: frequency }),
       setUser: (user) => 
         set({ user }),
-      clearUser: () => 
-        set({ user: null })
+      setHouseworkFrequency(type, frequency) {
+        set((state) => ({
+          houseworkFrequencies: {
+            ...(state.houseworkFrequencies || {}),
+            [type]: frequency
+          }
+        }))
+      },
+      clearUser: () => set({ 
+        user: null,
+        mealFrequency: null,
+        houseworkFrequencies: null
+      })
     }),
     { name: "user-storage" }
   )

ef6448fe8d862de9cad925cb48b07c543554f38f
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-03 18:33:11 +0900

    [other] temporary commit

--- front/src/app/signup/page.tsx
+++ front/src/app/signup/page.tsx
@@ -16,55 +16,57 @@ export default function Signup() {
   };
 
   return (
-    <main>
-      <h1>新規会員登録</h1>
-      <form
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <div>
-          <label 
-            htmlFor="email"
-          >
-            メールアドレス
-          </label>
-          <input 
-            id="email"
-            type="email"
-            className="border"
-            autoComplete="off"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label 
-            htmlFor="password"
-          >
-            パスワード
-          </label>
-          <input 
-            id="password"
-            type="password"
-            className="border"
-            autoComplete="off"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button
-          type="button"
-          className="bg-blue-500 text-white"
-          onClick={handleSubmit}
+    <main className="flex items-center justify-center">
+      <div className="w-82">
+        <h1 className="text-center text-2xl">
+          新規会員登録
+        </h1>
+        <form
+          onSubmit={(e) => e.preventDefault()}
         >
-          登録
-        </button>
-      </form>
-
-      {isPending && (
-        <p>処理中...</p>
-      )}
-
-      {isError && (
-        <p>エラーが発生しました {error.message}</p>
-      )}
+          <div>
+            <label
+              htmlFor="email"
+            >
+              メールアドレス
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="border"
+              autoComplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+            >
+              パスワード
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="border"
+              autoComplete="off"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="button"
+            className="bg-blue-500 text-white"
+            onClick={handleSubmit}
+          >
+            登録
+          </button>
+        </form>
+        {isPending && (
+          <p>処理中...</p>
+        )}
+        {isError && (
+          <p>エラーが発生しました {error.message}</p>
+        )}
+      </div>
     </main>
   )
 }

fa3451c72d4f5c473fa93d7d51be8e9aee5318d9
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-05 02:44:39 +0900

    [add] HouseworkDetailOverviewコンポーネントの作成

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -7,10 +7,10 @@ import { CalendarSheet } from "@/components/features/calendar";
 import { Play, X } from "lucide-react";
 import { useGetHousework, useStartHousework, useEndHousework } from "@/hooks/features/housework";
 import { useGetCalender, useGetCalenderDate } from "@/hooks/features/calender/use-calenders";
-import { calculateTimeDifference, getHouseworkStatusById, getHouseworkDatesFromCalendar, formatDateToYMD, formatDateToYMDHMS } from "@/lib/utils/";
-import { getCaloriesByHouseworkAndLevel } from "@/lib/utils/housework";
-import { HouseworkStatusBadge } from "@/components/features/housework";
+import { getHouseworkDatesFromCalendar, formatDateToYMD, formatDateToYMDHMS } from "@/lib/utils/";
+import { HouseworkDetailOverview } from "@/components/features/housework";
 import { useHouseworkStore } from "@/stores/housework-store";
+import { AnimatePresence } from "framer-motion";
 
 export default function App() {
   const [date, setDate] = useState<Date | undefined>(undefined)
@@ -72,15 +72,13 @@ export default function App() {
     });
   };
 
-
-
   return (
-    <main>
+    <main className="bg-gray-500 w-screen h-screen">
       <div className="flex flex-col gap-4">
         {HOMEWORK_ITEMS.map((item) => (
           <Button
             key={item.id}
-            className="border"
+            className="border w-30"
             onClick={() => setSelectedId(item.id)}
           >
             {item.label}
@@ -88,57 +86,20 @@ export default function App() {
         ))}
       </div>
 
-      {selectedId && (
-        <div className="mt-4">
-          {housework && (() => {
-            const label = calculateTimeDifference(housework.doneAt); // 最後に作業した日
-            const status = getHouseworkStatusById(housework.doneAt, housework.houseworkId); // 現在の状態
-            const calorie = getCaloriesByHouseworkAndLevel(housework.houseworkId, status); // 消費カロリー
+      <AnimatePresence>
+        {selectedId && (
+          <HouseworkDetailOverview
+            selectedId={selectedId}
+            housework={housework}
+            isHouseworkRunning={isHouseworkRunning()}
+            onHouseworkStartAction={handleHouseworkStart}
+            onHouseworkEndAction={handleHouseworkEnd}
+            onHiddenAction={() => setSelectedId(null)}
+          />
+        )}
+      </AnimatePresence>
 
-            return (
-              <div>
-                <h2>
-                  {HOMEWORK_ITEMS[Number(housework.houseworkId) - 1].label}
-                </h2>
-                {!isHouseworkRunning() ? (
-                  <>
-                    <Button 
-                      className="border"
-                      onClick={() => handleHouseworkStart(Number(housework.houseworkId))}
-                    >
-                      <Play />
-                      作業開始
-                    </Button>
-                    <div>
-                      <h3>最後に作業した日</h3>
-                      <p>{label}</p>
-                    </div>
-                    <div>
-                      <h3>現在の状態</h3>
-                      <HouseworkStatusBadge 
-                        lastDoneDate={housework.doneAt}
-                        houseworkId={housework.houseworkId}
-                      />
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <Button 
-                      className="border"
-                      onClick={() => handleHouseworkEnd(Number(housework.houseworkId), calorie)}
-                    >
-                      <X />
-                      作業終了
-                    </Button>
-                  </>
-                )}
-              </div>
-            );
-          })()}
-        </div>
-      )}
-
-      <div>
+      {/* <div>
         <h2>Total Calorie: {calenderData?.totalCalorie}</h2>
         {isMounted && (
           <Calendar
@@ -148,7 +109,7 @@ export default function App() {
             houseworkDates={houseworkDates}
           />
         )}
-      </div>
+      </div> */}
 
       {calenderDate?.date && (
         <CalendarSheet 
--- front/src/components/features/housework/housework-status-badge.tsx
+++ front/src/components/features/housework/housework-status-badge.tsx
@@ -133,7 +133,7 @@ export function HouseworkStatusBadge({
   return (
     <div
       className={cn(
-        "inline-flex items-center gap-2 px-3 py-1.5 text-[20px] font-medium transition-colors",
+        "inline-flex items-center gap-2 py-1.5 text-[20px] font-medium transition-colors",
         colorClass,
         className
       )}

62608afb7c13798fb17db9030bfb117a2fb2a12b
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-05 06:32:29 +0900

    [fix] / 消す！

--- back/config/cors.php
+++ back/config/cors.php
@@ -18,7 +18,7 @@ return [
   'allowed_methods' => ['*'],
 
   'allowed_origins' => [
-    env('FRONTEND_URL', 'https://rizap-app.vercel.app/'),
+    env('FRONTEND_URL', 'https://rizap-app.vercel.app'),
   ],
 
   'allowed_origins_patterns' => [],

b589ea4b58b78c1c0393e1aa3294addf041a1d40
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-04 17:32:40 +0900

    [add] homework-itemsの作成

---
+++ front/src/data/homework-items.ts
@@ -0,0 +1,35 @@
+interface HomeworkItemsType {
+  id: string;
+  label: string;
+}
+
+export const HOMEWORK_ITEMS: HomeworkItemsType[] = [
+  {
+    id: "1",
+    label: "洗い物"
+  },
+  {
+    id: "2",
+    label: "洗濯"
+  },
+  {
+    id: "3",
+    label: "ゴミ捨て"
+  },
+  {
+    id: "4",
+    label: "風呂掃除"
+  },
+  {
+    id: "5",
+    label: "トイレ掃除"
+  },
+  {
+    id: "6",
+    label: "床掃除"
+  },
+  {
+    id: "7",
+    label: "整理整頓"
+  }
+];

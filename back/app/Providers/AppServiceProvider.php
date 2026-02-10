759223571c694a4351b326ae1c54338932c5df05
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-10 18:56:01 +0900

    Fix: Remove Artisan::call from boot() causing infinite loop

--- back/app/Providers/AppServiceProvider.php
+++ back/app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,9 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            Artisan::call('config:clear');
-            Artisan::call('config:cache');
-        }
+        //
     }
 }
